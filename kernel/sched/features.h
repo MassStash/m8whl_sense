aad44cd5d592266c45e99394cc2e3a92001bb371
Thicklizard <thicklizard@gmail.com>
2014-04-14 09:18:22 -0500

    disable gentle fair sleepers for better performance on android
    Signed-off-by: MassStash <MassStash@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -1,9 +1,4 @@
-/*
- * Only give sleepers 50% of their service deficit. This allows
- * them to run sooner, but does not allow tons of sleepers to
- * rip the spread apart.
- */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
 
 /*
  * Place new tasks ahead so that they do not starve already running
