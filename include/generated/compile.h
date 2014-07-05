f907dfc8bbcf671758614883a736189aa5125aba
MassStash <MassStash@gmail.com>
2014-07-05 00:07:12 -0500

    Add some built shit
    Signed-off-by: MassStash <MassStash@gmail.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 31 */
+/* This file is auto generated, version 38 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#31 SMP PREEMPT Mon Jun 23 13:19:54 CDT 2014"
+#define UTS_VERSION "#38 SMP PREEMPT Fri Jul 4 23:34:37 CDT 2014"
 #define LINUX_COMPILE_BY "MassStash"
 #define LINUX_COMPILE_HOST "zaichron-lin-pc"
 #define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2665 - Linaro GCC 2014.06 - Cortex-A15) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-MassStashedX-0.19.1"
+#define UTS_RELEASE "3.4.0-MassStashedX-0.19.2-beta"
