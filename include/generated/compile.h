d4ac06c4bc6cf69ac0bbca4e5f15fdf12c9060fb
MassStash <MassStash@gmail.com>
2014-07-09 18:11:44 -0500

    Remove build BS
    Signed-off-by: MassStash <MassStash@gmail.com>

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 44 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#44 SMP PREEMPT Mon Jul 7 23:59:42 CDT 2014"
-#define LINUX_COMPILE_BY "MassStash"
-#define LINUX_COMPILE_HOST "zaichron-lin-pc"
-#define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2665 - Linaro GCC 2014.06 - Cortex-A15) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.13-MassStashedX-0.19.2-beta"
