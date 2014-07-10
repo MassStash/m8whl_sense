57e9a0b71f5ea8b905187a80e66637e07c9b8d3b
MassStash <MassStash@gmail.com>
2014-07-10 03:53:06 -0500

    Add -Wno-unused-variable to makefile and head off future build borks, and rm built BS from repo not needed
    Signed-off-by: MassStash <MassStash@gmail.com>

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 40 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#40 SMP PREEMPT Sat Jul 5 00:58:40 CDT 2014"
-#define LINUX_COMPILE_BY "MassStash"
-#define LINUX_COMPILE_HOST "zaichron-lin-pc"
-#define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2665 - Linaro GCC 2014.06 - Cortex-A15) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.4.0-MassStashedX-0.19.2-beta"
