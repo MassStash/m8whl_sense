44ab82146185b9eadeca7017695eeca42c56af10
MassStash <MassStash@gmail.com>
2014-07-24 00:25:33 -0500

    Fixes to smp: patches from mainline 3.5 to hopefully help with hotplug efficiency, AKA delete a couple lines.
    Signed-off-by: MassStash <MassStash@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -244,12 +244,10 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen)
 				__func__, cpu);
 		goto out_release;
 	}
-	smpboot_park_threads(cpu);
 
 	err = __stop_machine(take_cpu_down, &tcd_param, cpumask_of(cpu));
 	if (err) {
 		/* CPU didn't die: tell everyone.  Can't complain. */
-		smpboot_unpark_threads(cpu);
 		cpu_notify_nofail(CPU_DOWN_FAILED | mod, hcpu);
 		goto out_release;
 	}
@@ -331,9 +329,6 @@ static int __cpuinit _cpu_up(unsigned int cpu, int tasks_frozen)
 	if (ret != 0)
 		goto out_notify;
 	BUG_ON(!cpu_online(cpu));
-
-	/* Wake the per cpu threads */
-	smpboot_unpark_threads(cpu);
 
 	/* Now call notifier in preparation. */
 	cpu_notify(CPU_ONLINE | mod, hcpu);
